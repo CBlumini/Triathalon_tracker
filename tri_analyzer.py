835fcad23985bc2d2c81f98118374e9d2ad9d8c9
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-09-28 23:06:34 -0700

    First Pass

---
+++ tri_analyzer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 21:39:55 2021
+
+@author: Charlie
+"""
+
+import pandas as pd
+
+data = pd.read_excel('santa_cruz_data.xlsx', header = 1, index_col=0)
