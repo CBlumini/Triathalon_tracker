1984df8fc792d3b4f90d4c4c19c4a5771d43d858
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-12-06 20:09:34 -0800

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -5,30 +5,10 @@ from dash import html
 from dash.dependencies import Input, Output
 from dash import dash_table
 import pandas as pd
-import sqlite3
 from sqlalchemy import create_engine
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
-#import plotly.graph_objs as go
-engine = create_engine('sqlite:///data.db', echo=False)
-sql_df = pd.read_sql('data.db', con=engine, index_col='ID')
-
-#df = df[['country', 'description', 'rating', 'price','province','title','variety','winery','color']]
-print(sql_df.head(1))
-
-app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
-server = app.server
-app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div([
-    dcc.Graph(figure=fig)])
-
-#set the app.layout
-# app.layout = html.Div([
-#     dcc.Tabs(id="tabs", value='tab-1', children=[
-#         dcc.Tab(label='Tab one', value='tab-1'),
-#         dcc.Tab(label='Tab two', value='tab-2'),
-#     ]),
-#     html.Div(id='tabs-content')
-# ])
-
-print("hello")
+data = pd.read_excel('santa_cruz_data.xlsx', header = 0, index_col=None)
+print(data.head())
